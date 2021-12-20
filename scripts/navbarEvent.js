90d757027aba410e7af4101cc0cb99b1ecc4b038
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-20 10:47:31 -0300

    feat: back to top button (desktop)

--- scripts/navbarEvent.js
+++ scripts/navbarEvent.js
@@ -1,9 +1,11 @@
 async function depthNavBar() {
     if (window.scrollY > 0) {
         document.querySelector("nav").style.background = 'rgba(0, 0, 0, 0.7)';
+        document.querySelector("#topback").style.display = 'flex';
     }
     else {
         document.querySelector("nav").style.background = 'transparent';
+        document.querySelector("#topback").style.display = 'none';
     }
 
     await new Promise(r => setTimeout(r, 100));
